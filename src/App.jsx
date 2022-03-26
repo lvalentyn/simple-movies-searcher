1ee0886ec12b70d5a63732016e489b9fa2fc97ef
Ewig <valikallstars@gmail.com>
2022-03-26 20:40:46 +0200

    added simple search component

---
+++ src/App.jsx
@@ -0,0 +1,49 @@
+import './App.css';
+import SearchIcon from './search.svg';
+
+import { useEffect, useState } from 'react';
+import MovieCard from './MovieCard';
+
+const API_URL = 'http://www.omdbapi.com?apikey=fbf89529';
+
+const App = () => {
+	const [movies, setMovies] = useState([]);
+	const [searchTerm, setSearchTerm] = useState('');
+
+	const searchMovies = async (title) => {
+		const response = await fetch(`${API_URL}&s=${title}`);
+		const data = await response.json();
+
+		setMovies(data.Search)
+	}
+
+	useEffect(() => {
+		searchMovies('batman');
+	}, [])
+
+	return (
+		<div className="app">
+			<h1>MovieLand</h1>
+
+			<div className="search">
+				<input type="text" placeholder='Search for movies' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
+				<img src={SearchIcon} alt="search" onClick={() => searchMovies(searchTerm)} />
+			</div>
+
+			{movies?.length > 0
+				? (
+					<div className="container">
+						{movies.map((item, i) => (
+							<MovieCard key={i} movie={item} />
+						))}
+					</div>
+				) : (
+					<div className="empty">
+						<h2>No movies found</h2>
+					</div>
+				)}
+		</div>
+	);
+}
+
+export default App;
